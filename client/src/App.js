dc420f998537833774f96942e0c3e1501bc7e52b
Yasir Affan <yasiraffan9@gmail.com>
2020-07-14 00:13:38 +0530

    added login and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+import signup from "./pages/signup";
 import React from "react";
 import "./App.css";
 
 function App() {
-	return <div>Hello World</div>;
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route exact path="/login" component={login} />
+					<Route exact path="/signup" component={signup} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 export default App;
