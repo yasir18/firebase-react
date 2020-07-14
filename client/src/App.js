7ab6f4e7e62ce0407ce066e5edef0b56b2888252
Yasir Affan <yasiraffan9@gmail.com>
2020-07-14 22:50:25 +0530

    added home and account components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import home from "./pages/home";
 import React from "react";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
 		<Router>
 			<div>
 				<Switch>
+					<Route exact path="/" component={home} />
 					<Route exact path="/login" component={login} />
 					<Route exact path="/signup" component={signup} />
 				</Switch>
